a831682afdad87d63ccef72de2a07bdae0464e41
darlingson <dmakuwila@gmail.com>
2026-05-23 22:34:46 +0200

    connect app to postgres db

--- main.go
+++ main.go
@@ -6,6 +6,16 @@ import (
     "io"
     "net/http"
     "os"
+    "database/sql"
+    _ "github.com/lib/pq"
+)
+
+const (
+  host     = "localhost"
+  port     = 5432
+  user     = "postgres"
+  password = "masterpassword"
+  dbname   = "oort_objects"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
@@ -18,9 +28,20 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "password=%s dbname=%s sslmode=disable",
+    host, port, user, password, dbname)
+
+    db, err := sql.Open("postgres", psqlInfo)
+    if err != nil {
+        panic(err)
+    }
+    defer db.Close()
+
     http.HandleFunc("/", getRoot)
     http.HandleFunc("/hello", getHello)
-    err := http.ListenAndServe(":3333", nil)
+    err = http.ListenAndServe(":3333", nil)
     if errors.Is(err, http.ErrServerClosed) {
         fmt.Printf("server closed\n")
     } else if err != nil {
